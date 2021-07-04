4ad658a06b86935eb053abb2ff83826e770cc9e4
Tran Quoc Tuyen <tuyentq.1997@gmail.com>
2021-07-04 10:51:10 +0700

    calculateParkingFee func.

---
+++ problem1-calcuate-packing-fee.js
@@ -0,0 +1,33 @@
+'use strict';
+
+Date.prototype.isValid = function () {
+    return this.getTime() === this.getTime();
+};
+
+const packingPriceDaily = 5;
+const timeInOneDay = 86400000;
+const calculateParkingFee = (timeIn) => {
+    if (!timeIn.isValid()) {
+        throw new Error('Invalid Date');
+    }
+
+    const startDate = new Date(timeIn);
+
+    if (startDate.getTime() >= Date.now()) {
+        throw new Error('Invalid Date');
+    }
+
+    let totalDays = (Date.now() - startDate.getTime()) / timeInOneDay;
+
+    totalDays = Math.ceil(totalDays);
+
+    console.log(`Total parking fee is: ${totalDays * packingPriceDaily}$`)
+
+    return totalDays * packingPriceDaily;
+}
+
+// case: success
+const start = new Date();
+start.setDate(start.getDate() - 3);
+calculateParkingFee(start);
+
